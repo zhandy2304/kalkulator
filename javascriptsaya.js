3db4701ef0a5ef66251fbe9c328224b420eaed7b
Muhammad Zhandy <66477968+zhandy2304@users.noreply.github.com>
2022-07-08 22:20:47 +0800

    Add files via upload

---
+++ javascriptsaya.js
@@ -0,0 +1,19 @@
+function penjumlahan() {
+	var input1 = parseInt(document.getElementById("a").value);
+	var input2 = parseInt(document.getElementById("b").value);
+	document.getElementById("hasil").innerHTML = input1+input2;}
+
+function pengurangan() {
+	var input1 = parseInt(document.getElementById("a").value);
+	var input2 = parseInt(document.getElementById("b").value);
+	document.getElementById("hasil").innerHTML = input1-input2;}
+
+function pembagian() {
+	var input1 = parseInt(document.getElementById("a").value);
+	var input2 = parseInt(document.getElementById("b").value);
+	document.getElementById("hasil").innerHTML = input1/input2;}
+
+function perkalian() {
+	var input1 = parseInt(document.getElementById("a").value);
+	var input2 = parseInt(document.getElementById("b").value);
+	document.getElementById("hasil").innerHTML = input1*input2;}
